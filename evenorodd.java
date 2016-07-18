0f9d67838340524746f9445218d43ee723f057af
NaliniSekar <NaliniSekar@users.noreply.github.com>
2016-07-18 00:01:16 -0700

    Update and rename README.md to evenorodd.java

---
+++ evenorodd.java
@@ -0,0 +1,16 @@
+# evenorodd
+Public static void main(String args[])
+{
+System.out.println("enter num");
+Scanner in = new Scanner();
+int b=in.nextInt();
+if(b/2==0)
+{
+System.out.println("num is even");
+}
+else
+{
+System.out.println("num is odd");
+}
+}
+
